70cc8a2454f045d35101f0fd07ea9d1da7b05ec3
ryudonghun78 <ryudonghun78@gmail.com>
2017-11-23 10:24:41 +0900

    10172 update

---
+++ Backjoon/Backjoon/10172.cpp
@@ -0,0 +1,19 @@
+//
+//  10172.cpp
+//  Backjoon
+//
+//  Created by Dongheon Ryu on 2017. 11. 23..
+//  Copyright © 2017년 Dongheon Ryu. All rights reserved.
+//
+
+#include<stdio.h>
+
+int main(){
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+    return 0;
+}
+

c8d1e740419f5b20d9295c5b4ee8a32721641487
ryudonghun78 <ryudonghun78@gmail.com>
2018-01-20 00:38:02 +0900

    10809 - 알파벳 찾기
    10808문제에서 조금만 변형해도 풀 수 있는 문제였다. 그 알파벳의 인덱스를 찾는 문제였으므로 그리고 첫번째 나온 위치를 출력하는 것이 정답이었으므로 첫번째 출력 위치에서 break를 통해 빠져나올 수 있었다.

--- 10809.cpp
+++ 10809.cpp
@@ -1,23 +1,30 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include<stdio.h>
 #include<string.h>
+
 using namespace std;
 
-int main() {
-	string s;
-	cin >> s;
-	int len = s.length();
-	for (int i = 'a'; i <='z'; i++) {
-		for (int j = 0; j < len; j++) {
-			if (i == s[j]) {
-				cout << j << " ";
+int main(){
+	char str[102];
+	scanf("%s",str);
+	int size;
+
+
+	size = strlen(str); //개행문자 포함 안함
+
+	for(int i='a';i<='z';i++){
+		int count=0;
+		int index=-1;
+		for(int j=0;j<size;j++){
+			if(i==str[j]){
+				index = count;
 				break;
 			}
-			else if (j == len-1) {
-				cout << -1 << " ";
-			}
+			count++;
 		}
+		printf("%d ",index);
+		count = 0;
+		index = -1;
 	}
 	return 0;
-};
+
+}

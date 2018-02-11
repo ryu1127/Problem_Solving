3797a2a03577592c0a948579959c94390e39033f
ryudonghun78 <ryudonghun78@gmail.com>
2018-02-11 16:35:51 +0900

    11651 - 좌표 정렬
    좌표를 정렬하는 방법 중 두번째 문제이다. 쉽게 다음 문제와 비교하여 거꾸로 저장해서 풀 수 있었다.

--- Sorting Algorithm/11651.cpp
+++ Sorting Algorithm/11651.cpp
@@ -1,8 +1,18 @@
 #include<stdio.h>
+#include<algorithm>
 
 int main() {
-	long long *a = new long long;
-	printf("%s", ((long long)a % 5 ? "Yonsei" : "Korea"));
-	delete a;
+	std::pair<int, int> a[100001];
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a[i].second, &a[i].first);
+	}
+	std::sort(a, a + n);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", a[i].second, a[i].first);
+	}
 	return 0;
+
+
 }

c488366e4d423cbe6fbab896580099d7354a164a
ryudonghun78 <ryudonghun78@gmail.com>
2018-02-14 22:41:06 +0900

    11652 - 카드
    카드 문제는 계속해서 헷갈렸지만 예전에 했던 다이나믹 프로그래밍과 비슷하게 풀 수 있었다.
    
    그리고 여기서 가장 헷갈렸던 부분은 결국 result  부분이었다.
    result부분을 제대로 처리하지 못해 계속 80~90퍼까지가고 틀렸습니다가 떴다.
    
    손코딩을 통해 하나씩 차근차근 반례를 돌려보는 것이 중요할 것 같다.

---
+++ Sorting Algorithm/11652.cpp
@@ -0,0 +1,43 @@
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+long long a[1000001] = { 0 };
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	vector<long long> v;
+	long long index;
+	for (int i = 0; i < n; i++) {
+		scanf("%lld", &index);
+		v.push_back(index);
+	}
+	if (n == 1) {
+		printf("%lld", index);
+		return 0;
+	}
+	sort(v.begin(), v.end());
+
+	int max = 1;
+	int count = 1;
+	long long result = v[0];
+	for (int i = 0; i<n-1; i++) {
+		if (v[i] == v[i + 1]) count++;
+		else {
+			if(max<count){
+				result = v[i];
+				max = count;
+			}
+			count = 1;
+		}
+		if (max < count) {
+			max = count;
+			result = v[i];
+		}
+	}
+	printf("%lld\n", result);
+	return 0;
+}

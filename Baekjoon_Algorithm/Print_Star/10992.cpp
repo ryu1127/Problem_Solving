9c1fc1f55df1e5028e7ed3243433bdac0d31cc9f
ryudonghun78 <ryudonghun78@gmail.com>
2019-05-12 02:17:15 +0900

    10992 - 별찍기 - 17
    앞의 16과 비슷하지만 마지막 줄만 모두 출력하는게 달랐다.
    그리고 중간 부분도 출력을 하지 않아야 했기 떄문에 그 부분을 처리하는 것이 조금 까다로웠지만 어려운 문제는 아니었다.

---
+++ Baekjoon_Algorithm/Print_Star/10992.cpp
@@ -0,0 +1,34 @@
+﻿#include<cstdio>
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	int i;
+	cin >> n;
+	if (n == 1) printf("*");
+	else {
+		for (int line = 1; line <= n; line++) {
+			for (i = 1; i <= n - line; i++) {
+				printf(" ");
+			}
+			printf("*");
+			//1+2*(line-2)
+			if (line != n) {
+				for (i = 0; i < (1 + 2 * (line - 2)); i++) { 
+					printf(" "); 
+				}
+				if(line!=1)	printf("*");
+			}
+			else {
+				for (i = 0; i < (2*n)-2; i++) {
+					printf("*");
+				}
+			}
+
+			if (line != n) printf("\n");
+		}
+	}
+	return 0;
+}

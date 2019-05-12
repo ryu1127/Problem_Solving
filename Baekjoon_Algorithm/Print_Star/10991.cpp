030e0e919f7c513ff560652629de637083830513
ryu <ryudonghun78@gmail.com>
2019-05-12 20:05:37 +0900

    10991 - 별찍기 - 16
    간단한 문제였다.

---
+++ Baekjoon_Algorithm/Print_Star/10991.cpp
@@ -0,0 +1,21 @@
+#include<cstdio>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int i;
+    cin >> n;
+    for(int line = 1; line <= n; line++){
+        for(i=1;i<=n-line;i++){
+            printf(" ");
+        }
+        printf("*");
+        for(i=0;i<line-1;i++){
+            printf(" *");
+        }
+    }
+    return 0;
+
+}
